c8fd9a81d4b4057de76c9730e1169acb6e24f853
ctreidoshark <reidoshark@hotmail.com>
2026-04-20 23:48:12 -0300

    att

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/futevolei/",
+  base: "/ctreidoshark/",
   plugins: [react()],
 });
